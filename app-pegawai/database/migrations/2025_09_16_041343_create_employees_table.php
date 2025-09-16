345f0ce4c6a94bc461a1a33df7c62f4353964e99
Rotioboy <bangrael89@gmail.com>
2025-09-16 11:21:49 +0700

    Konfigurasi Awal Projek

---
+++ app-pegawai/database/migrations/2025_09_16_041343_create_employees_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_lengkap', 100);
+            $table->string('email', 100);
+            $table->string('nomor_telepon', 15);
+            $table->date('tanggal_lahir');
+            $table->text('alamat');
+            $table->date('tanggal_masuk');
+            $table->enum('status', ['aktif','nonaktif'])->default('aktif');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
